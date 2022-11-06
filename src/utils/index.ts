90c5dd299945284ac973037dd8159f2e5ca8029b
Nathan Silva Santos <nathan@w16.com.br>
2022-11-06 14:37:34 -0300

    initial commit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,5 +3,4 @@ import formatDate from './formatDate';
 import removeSpecialCharacters from './removeSpecialCharacters';
 import sortArray from './sortArray';
 
-
 export { debounce, formatDate, removeSpecialCharacters, sortArray };
