e47d7f2d2f806ae346c5bb968217676bfb0c3735
Nathan Silva Santos <nathan@w16.com.br>
2022-11-30 00:14:22 -0300

    prettier eslit rule

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,6 +3,7 @@ import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
 import '@fontsource/roboto-mono/300.css';
 import '@fontsource/roboto-mono/400.css';
 import '@fontsource/roboto-mono/500.css';
